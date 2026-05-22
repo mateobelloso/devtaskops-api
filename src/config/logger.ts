25f312a966793fa7d71f12bc05e1583ef00041dd
Mateo Belloso <mateobp97@alu.ing.umlp.edu.ar>
2026-05-22 20:21:32 -0300

    Agregue el docker build

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,12 +1,13 @@
 import pino from 'pino';
 
-const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true
-    }
-  }
-});
+const logger = pino(
+  process.env.NODE_ENV === 'development'
+    ? {
+        transport: {
+          target: 'pino-pretty',
+        },
+      }
+    : {}
+);
 
 export default logger;
